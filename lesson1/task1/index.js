18434c5a3c47145419ba03927390f2ff7845cdb6
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-20 15:01:14 +0200

    hello-react

--- lesson1/task1/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-const rootElement = document.querySelector('#root');
-
-const greetingElem = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
-
-ReactDOM.render(greetingElem, rootElement);
