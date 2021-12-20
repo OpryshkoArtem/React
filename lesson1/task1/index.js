f32015ae3f37915438cb9e51612c934ef89cec5f
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-20 16:02:36 +0200

    ok

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const getRootElem = document.querySelector('#root');
 
 const greetingElem = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
--- lesson1/task2/index.js
+++ lesson1/task2/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const rootElement = document.querySelector('#root');
 
 const element = React.createElement(
