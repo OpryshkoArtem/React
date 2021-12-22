f8188e2ff1b28b2a607ec1e6e5b2a1e038b24c7f
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-23 00:07:21 +0200

    task3 done

---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import Comment from './Comment';
+import App from './App';
+
+const rootElement = document.querySelector('#root');
+
+
+
+ReactDOM.render(
+	<App />,
+	rootElement
+);
