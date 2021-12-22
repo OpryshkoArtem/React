514891be94da9233ac60e2fcfae4511ba0da2822
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-23 00:19:50 +0200

    fix author

--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -5,9 +5,9 @@ const UserInfo = props => {
 	return (
 		<div className="user-info">
 			<UserAvatar
-				avatarUrl={props.user.avatarUrl}
-				name={props.user.name} />
-    	<div className="user-info__name">{props.user.name}</div>
+				avatarUrl={props.author.avatarUrl}
+				name={props.author.name} />
+    	<div className="user-info__name">{props.author.name}</div>
     </div>
 	);
 };
--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
-import Comment from './Comment';
 import App from './App';
 
 const rootElement = document.querySelector('#root');
