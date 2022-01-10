6b6541387bfd398b47bbf38c22cfadb3bf124f84
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-01-10 19:11:33 +0200

    task2

--- lesson7/task2/src/UsersList.jsx
+++ lesson7/task2/src/UsersList.jsx
@@ -17,7 +17,7 @@ class UsersList extends Component {
     let usersList;
     if (this.state.sorting) {
       usersList = this.props.users.sort((a, b) =>
-        this.state.sorting === 'asc' ? a.age - b.age : b.age - a.age,
+        this.state.sorting === 'asc' ? b.age - a.age : a.age - b.age,
       );
     } else {
       usersList = this.props.users;
