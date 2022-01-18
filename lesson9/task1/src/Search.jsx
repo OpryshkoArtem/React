66b18ced7ee07d2bf24cc190679f426e989cb90f
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-01-18 18:21:49 +0200

    lesson9 task1

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = { value: '' };
+
+  handleChange = event => {
+    console.log(event.target.value);
+    this.setState({ value: event.target.value });
+  };
+
+  search = event => {
+    event.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="search" onSubmit={this.search}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.value}
+            className="search__input"
+          />
+          <button className="search__button" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
