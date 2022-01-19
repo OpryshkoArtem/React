6bf6fddc7f45c26b9b1c6a3ef760b0141af2b2d8
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-01-19 12:11:04 +0200

    task4

--- lesson9/task4/src/index.jsx
+++ lesson9/task4/src/index.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App.jsx';
+import UsersList from './UsersList.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+const users = [
+  {
+    name: 'Frodo',
+    age: 44,
+    id: 'id-1',
+  },
+  {
+    name: 'Gans',
+    age: 30,
+    id: 'id-2',
+  },
+  {
+    name: 'Anna',
+    age: 23,
+    id: 'id-3',
+  },
+  {
+    name: 'Robert',
+    age: 12,
+    id: 'id-4',
+  },
+  {
+    name: 'Barbara',
+    age: 22,
+    id: 'id-5',
+  },
+  {
+    name: 'Alice',
+    age: 50,
+    id: 'id-6',
+  },
+];
+
+ReactDOM.render(<UsersList users={users} />, rootElement);
