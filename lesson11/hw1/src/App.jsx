03719988e17140afe7c1a550eeeedde4368bd70b
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-01-22 23:41:16 +0200

    foggot div

--- lesson11/hw1/src/App.jsx
+++ lesson11/hw1/src/App.jsx
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import Expand from './Expand.jsx';
 
 class App extends Component {
-
   render() {
     return (
-      <Expand title="Some title">
-        <p>
-          Hooks are a new addition in React 16.8. They let you use state and other React features
-          without writing a class.
-        </p>
-      </Expand>
+      <div class="app">
+        <Expand title="Some title">
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and other React features
+            without writing a class.
+          </p>
+        </Expand>
+      </div>
     );
   }
 }
