07f3a4df602b98c8b91f49689cf7366beb5d0a0f
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-01-23 12:31:08 +0200

    part2

--- lesson12/task1/src/CreateTaskInput.jsx
+++ lesson12/task1/src/CreateTaskInput.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CreateTaskInput = () => {
-  return (
-    <div className="create-task">
-      <input className="create-task__input" type="text" />
-      <button className="btn create-task__btn">Create</button>
-    </div>
-  );
-};
+class CreateTaskInput extends Component {
+	state = {
+		value: '',
+	};
+
+	handleChange = event => {
+		this.setState({
+			value: event.target.value,
+		})
+	}
+
+	handleTaskCreate = () => {
+		this.props.onCreate(this.state.value);
+		this.setState({ value: '' })
+	}
+
+  render() {
+    return (
+      <div className="create-task">
+        <input
+					className="create-task__input"
+					type="text"
+					alue={this.state.value}
+					onChange={this.handleChange}
+				/>
+        <button className="btn create-task__btn" onClick={this.handleTaskCreate}>Create</button>
+      </div>
+    );
+  }
+}
 
 export default CreateTaskInput;
+// взяти текст з інпута
+// створити таску з цим текстом
+// добавити таску в список
